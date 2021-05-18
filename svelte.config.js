37501ed89f7afc3bb9895e2be881b9cd5b22357d
Ronan Cotillard <Goodzilla@users.noreply.github.com>
2021-05-18 16:40:28 +0200

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -4,9 +4,9 @@ import vercel from '@sveltejs/adapter-vercel';
 const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
-	},
-	adapter: vercel()
+		target: '#svelte',
+		adapter: vercel()
+	}
 };
 
 export default config;
