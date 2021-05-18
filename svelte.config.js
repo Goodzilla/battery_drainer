13b9b93f36388a4d2a140fcab98dd4b8d3dd24c8
Ronan Cotillard <Goodzilla@users.noreply.github.com>
2021-05-18 16:28:34 +0200

    Ajout adaptateur Vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,12 @@
+const vercel = require('@sveltejs/adapter-vercel');
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
-	}
+	},
+	adapter: vercel()
 };
 
 export default config;
